4ca23bc4ce7f39ca7c3eea8191b78658a770b615
Ethan <131805068+corynko@users.noreply.github.com>
2023-05-29 18:27:23 -0600

    added local storage functionality for action items and save buttons, added personalization

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,22 @@
-var dayEl = $("#currentDay");
-var timeEl = $("#currentTime");
-var currentHour = dayjs().hour();
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-dayEl.text("Today is " + dayjs().format("dddd, MMMM DD, YYYY"));
-timeEl.text("-- it is  " + dayjs().format("hh:mm a --"));
-
-console.log(currentHour);
-
 $(function () {
+  //general time variables available to the full function
+  var nameEl = $("#welcome");
+  var dayEl = $("#currentDay");
+  var timeEl = $("#currentTime");
+  var currentHour = dayjs().hour();
+  // var currentHour = 13;
+
+  //changes styling based on current hour
   var timeBlocks = $(".time-block");
   var blockText = $(".description");
-  console.log(timeBlocks);
+
+  //sanity checks are commented out
+  // console.log(timeBlocks);
+  // console.log(currentHour);
   for (var i = 0; i < timeBlocks.length; i++) {
     var boxHour = Number(timeBlocks[i].id);
-    console.log(boxHour);
+    // console.log(boxHour);
+
     if (boxHour < currentHour) {
       // console.log("I'm past!");
       blockText[i].classList.add("past");
@@ -29,22 +28,33 @@ $(function () {
       blockText[i].classList.add("future");
     }
   }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  renderSaved();
+
+  // saves input into local storage as specified by hour
+  var saveEl = $(".saveBtn");
+  saveEl.on("click", function () {
+    var parentID = this.parentElement.id;
+    var input = this.previousElementSibling.value;
+    // console.log(parentID);
+    // console.log(input);
+    window.localStorage.setItem(parentID, input);
+  });
+
+  function renderSaved() {
+    for (var i = 0; i < timeBlocks.length; i++) {
+      var boxHour = Number(timeBlocks[i].id);
+      timeBlocks[i].children[1].value = window.localStorage.getItem(boxHour);
+    }
+  }
+
+  //handles name banner
+  if (window.localStorage.getItem("name") == null) {
+    var name = window.prompt("Hello! What's Your Name?");
+    window.localStorage.setItem("name", name);
+  }
+
+  nameEl.text("Welcome Back, " + window.localStorage.getItem("name"));
+  dayEl.text("Today is " + dayjs().format("dddd, MMMM DD, YYYY"));
+  timeEl.text("-- it is  " + dayjs().format("hh:mm a --"));
 });
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Work-Day-Planner
+
+## What Is It?
+
+A simple planner optimized for a 9-5 workday. Action items are saved in the user's local memory to be recalled upon page load, and the planner gives graphical feedback based on the current real time.
+
+## Where Is It?
+
+You can find and use the planner at [https://corynko.github.io/Work-Day-Planner](https://corynko.github.io/Work-Day-Planner)
+
+## How Do I Use It?
+
+First, fill in your name. Once you press ok, the calendar will load. Click on an hour and type in your tasks for that hour, then press the save button the right. Set this planner as your new tab page to pull it up more often during your day. The hour blocks will change color to communicate where you are in your day at a glance, as in the second screenshot taken during lunch.
+
+![A blank page in the planner](./Assets/images/image-1.png)
+![A snippet from the planner taken at lunch](./Assets/images/image-2.png)
+
+## Who Can Use It?
+
+This planner is completely open source and available to anyone to use or modify to suit your needs.
